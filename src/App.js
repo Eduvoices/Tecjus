7bf371e6b12d9348f715d6a1d520e9bc74797c95
Luis Eduardo <eduvoices25@gmail.com>
2023-07-17 16:43:02 -0300

    adicionando a página de menu

--- src/App.js
+++ src/App.js
@@ -1,142 +1,105 @@
 import exit from './assets/log-out.svg'
+import activity from './assets/monitor.svg'
+import money from './assets/dollar-sign(1).svg'
+import user from './assets/user(1).svg'
+import building from './assets/home(1).svg'
+import file from './assets/archive(1).svg'
+import clock from './assets/clock.svg'
+import calendar from './assets/calendar(1).svg'
+import gift from './assets/gift(1).svg'
 import logo from './assets/Logo_page-0001-removebg-preview1.png'
-import file from './assets/file-text.svg'
-import search from './assets/search.svg'
-import InputMask from 'react-input-mask'
+
 
 import './styles.css'
-import { useState } from 'react'
 
 function App() {
-  const [mask, setMask] = useState('')
 
-  function Input() {
-    if (mask === 'cpf') {
-      return (
-        <InputMask mask='999.999.999-99' className='input' />
-      )
-    } else if (mask === 'data') {
-      return (
-        <InputMask mask='99/99/9999' className='input'/>
-      )
-    } else {
-      return (
-        <input className="input" id=''/>
-      )
-    }
-  }
-
+  let now = new Date()
+  let day = now.getDate().toString().padStart(2, '0')
+  let month = String(now.getMonth() + 1).padStart(2, '0')
+  let year = now.getFullYear()
 
   return (
+    <>
+    <header>
+                <nav className='nav-container'>
+                <ul className='list'>
+                  <li className='list-item'>
+                    <button className='list-anchor'>Cadastros</button>
+                  </li>
+                  <li className='list-item'>
+                    <button className='list-anchor'>Processos</button>
+                  </li>
+                  <li className='list-item'>
+                    <button className='list-anchor'>Finainceiro</button>
+                  </li>
+                  <li className='list-item'>
+                    <button className='list-anchor'>Consultas</button>
+                  </li>
+                  <li className='list-item'>
+                    <button className='list-anchor'>Sistema</button>
+                  </li>
+                  <li className='list-item'>
+                    <button className='list-anchor'>Ajuda</button>
+                  </li>
+                  <li className='list-item'>
+                    <img src={exit} alt='Sair do sistema'/>
+                  </li>
+                </ul>
+                </nav>
+    </header>
     <div className="container">
-            <div className="container-consulta">
-                <div className="wrapper">
-                  <img src={exit} alt='Sair do sistema' className='exit'/>
-                  <span  className="form-title">
-                    <img src={logo} alt="Logo do sistema"/>
-                  </span>
-
-                        <span className="form-title">
-                        Consulta
-                        </span>
-
-                      <form className="consulta-form">
-                        <div className="wrap-input">
-                            <Input className="input" id=''/>
-                            <span className='focus-input' data-placeholder='Informação de busca'></span>
-                            
-                        </div>
-
-                        <button type='button' className='search-btn'>
-                          <img src={search} alt='Pesquisar' />
-                        </button>
-
-                        <select className='wrap-select'>
-                          <option className='option' onClick={e => setMask('')}>Tipo</option>
-                          <option className='option' onClick={e => setMask('')}>Nome</option>
-                          <option className='option' onClick={e => setMask('cpf')}>CPF</option>
-                          <option className='option' onClick={e => setMask('data')}>Data de cadastro</option>
-                        </select>
-
-                        <select className='wrap-select'>
-                          <option className='option'>Tipo de consulta</option>
-                          <option className='option'>Advogado</option>
-                          <option className='option'>Cliente</option>
-                          <option className='option'>Funcionário</option>
-                          <option className='option'>Pessoa</option>
-                          <option className='option'>Parceiro</option>
-                          <option className='option'>Usuário</option>
-                        </select>
-                    </form>
-
-                    <div className='drag'>
-                      <p>
-                        Arraste um cabeçalho da coluna aqui para agrupar por essa coluna
-                      </p>
-                    </div>
-
-                    <table>
-                      <thead>
-                        <th>Código</th>
-                        <th>Nome</th>
-                        <th>CPF</th>
-                        <th>Rua</th>
-                        <th>Número</th>
-                        <th>Bairro</th>
-                        <th>Complemento</th>
-                        <th>CEP</th>
-                        <th>Cidade</th>
-                        <th>Data de nascimento</th>
-                      </thead>
-
-                      <tr>
-                        <td>1072</td>
-                        <td>Priscila Paiva Blasechi</td>
-                        <td>000.000.000-00</td>
-                        <td>Rua Teste do Teste Teste</td>
-                        <td>000</td>
-                        <td>Bairro</td>
-                        <td>Complemento</td>
-                        <td>00000-000</td>
-                        <td>Arapongas</td>
-                        <td>01/01/1010</td>
-                      </tr>
-                    </table>
-
-                    <table>
-                      <thead>
-                        <th>Telefone 1</th>
-                        <th>Telefone 2</th>
-                        <th>Telefone 3</th>
-                        <th>E-mail</th>
-                        <th>Responsável</th>
-                        <th>Rede Social</th>
-                        <th>Como ?</th>
-                        <th>Observação</th>
-                        <th>Data de Cadastro</th>
-                      </thead>
-
-                      <tr>
-                        <td>(99)99999-9999</td>
-                        <td>(99)99999-9999</td>
-                        <td>(99)99999-9999</td>
-                        <td>exemplo@exemplo.com</td>
-                        <td>Nome do Responsável</td>
-                        <td>@exemplo</td>
-                        <td>Complemento</td>
-                        <td>Exemplo</td>
-                        <td>01/01/1010</td>
-                      </tr>
-                    </table>
-
-                    <button type="submit" className='form-btn'>
-                            <img src={file} alt='Cadastro' />
-                              Cadastro
-                    </button>
-
-                    </div>
-            </div>
+              <main className='wrapper'>
+                <div className='button-row'>
+                  <button className='button-item' type='button'>
+                    <img src={activity} alt='Atendimentos' />
+                    Atendimentos
+                  </button>
+                  <button className='button-item' type='button'>
+                    <img src={user} alt='Pessoa Física' />
+                    Pessoa Física
+                  </button>
+                  <button className='button-item' type='button'>
+                    <img src={building} alt='Pessoa Jurídica' />
+                    Pessoa Jurídica
+                  </button>
+                  <button className='button-item' type='button'>
+                    <img src={file} alt='Processos' />
+                    Processos
+                  </button>
+                  <button className='button-item' type='button'>
+                    <img src={clock} alt='Andamentos' />
+                    Andamentos
+                  </button>
+                  <button className='button-item' type='button'>
+                    <img src={calendar} alt='Agenda' />
+                    Agenda
+                  </button>
+                  <button className='button-item' type='button'>
+                    <img src={money} alt='Faturar' />
+                    Faturar
+                  </button>
+                  <button className='button-item' type='button'>
+                    <img src={gift} alt='Aniversários' />
+                    Aniversários
+                  </button>
+                </div>
+                <div className='salute'>
+                  <p>Olá, seja bem vindo ao Tecjus!</p>
+                  <div className='birthday'>
+                    <p>Hoje é {day}/{month}/{year}.</p>
+                    <p>Temos aniversário!</p>
+                  </div>
+                </div>
+                <div className='central-area'>
+                  <img src={logo} alt='Logo do sistema'/>
+                  <p>ATENÇÃO! Processo monitorado com movimentação judicial. <br />
+                  Clique aqui para visualizar!
+                  </p>
+                </div>
+              </main>
     </div>
+    </>
   );
 }
 
